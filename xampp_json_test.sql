066483ddb2c1abeb4e68839d841edff8f9a1d978
gustavoruas <fabriziobrusca87@gmail.com>
2020-03-16 17:04:21 +0000

    Updating types and inserting in table

--- xampp_json_test.sql
+++ xampp_json_test.sql
@@ -9,12 +9,22 @@ DECLARE
  
  l_http_det_exc BOOLEAN;
  
+ --TBEX1_USER_JSON
+ --TBEX1_COLLECTION_JSON
+ TYPE t_rec_TBEX1_USER_JSON IS TABLE OF TBEX1_USER_JSON%ROWTYPE;
+ TYPE t_rec_TBEX1_COLLECTION_JSON IS TABLE OF TBEX1_COLLECTION_JSON%ROWTYPE;
+ 
+ --virtual tables
+ l_tab_user_json           t_rec_TBEX1_USER_JSON;
+ l_tab_collection_json     t_rec_TBEX1_COLLECTION_JSON;
+ 
+ l_user_id NUMBER;
  
  -- {"status":"","student_data":[{"name":"Flash Gordon","course":"Class lesson","level":"12","collections":{"books":"IGET","music":"Book of gg"}},{"name":"Edu","course":"Geography","level":"300","collections":{"books":"IAs","music":"GET22"}},{"name":"Guanga","course":"Hist","level":"123","collections":{"books":"IAs","music":"GET22"}},{"name":"Troid","course":"Guist","level":"22","collections":{"books":"IAs","music":"GET22"}}]}
  --PLJSON_CODE.json 
  l_json json := json();
  
- l_sub_json json := json();
+ l_json_values json := json();
  
  --Fetches element with []
  l_json_list json_list;
@@ -70,11 +80,7 @@ BEGIN
   IF l_value IS NOT NULL THEN
     l_json := PLJSON_CODE.json(l_value);  
     
-    --return amount of JSON elements
-    dbms_output.put_line('Count Elements: ' || l_json.count());    
-
-    dbms_output.put_line('Element: ' || l_json.get('student_data').get_string());
-    
+        
     --Default print from JSON in DBMS_OUTPUT
     --l_json.print();
     
@@ -84,7 +90,12 @@ BEGIN
     --Creating json_list with [] element
     l_json_list := json_list(l_json.get('student_data'));
     
-    dbms_output.put_line('  **ELement in [ ]: ' || l_json_list.count());
+        
+    --INITIALIZING type
+    l_tab_user_json := t_rec_TBEX1_USER_JSON();
+    l_tab_user_json.EXTEND(l_json_list.count());
+    
+    l_tab_collection_json := t_rec_TBEX1_COLLECTION_JSON();
     
     --Looping through elements
     FOR ind IN 1..l_json_list.count() LOOP
@@ -103,31 +114,186 @@ BEGIN
       
         --l_json_list.get(ind).print();
         --Insert element into JSON object
-        l_sub_json := json(l_json_list.get(ind));   
+        l_json_values := json(l_json_list.get(ind));   
         
         --Counts the amount of key pairs a json object has
-        DBMS_OUTPUT.PUT_LINE('Json element keys:' || l_sub_json.count());
+        DBMS_OUTPUT.PUT_LINE('Json element keys:' || l_json_values.count());
         
-        --Iterate within each index and value of JSON object
-        FOR ind1 IN 1..l_sub_json.count() LOOP  
+        --Aquiring ID for parent line
+        BEGIN        
+          SELECT SEQ_TBEX1_USER_JSON.NEXTVAL INTO l_user_id FROM DUAL;        
+        END;                              
+
+        l_tab_user_json(ind).user_id             := l_user_id;
                 
-          IF  l_sub_json.get(ind1).is_String THEN 
-            L_values_output := L_values_output || l_sub_json.get(ind1).get_string() || ' '  ;
+        --Iterate within each index and value of JSON object
+        FOR ind1 IN 1..l_json_values.count() LOOP  
+                
+          IF  l_json_values.get(ind1).is_String THEN 
+            L_values_output := L_values_output || l_json_values.get(ind1).get_string() || ' '  ;
           END IF;
           
-          IF l_sub_json.get(ind1).is_number THEN
-            l_values_output := L_values_output || l_sub_json.get(ind1).get_number() || ' '  ;
+          IF l_json_values.get(ind1).is_number THEN
+            l_values_output := L_values_output || l_json_values.get(ind1).get_number() || ' '  ;
+          END IF;
+          
+          --Populating type cols fro json object, matching per order element
+          CASE ind1
+            WHEN 1 THEN
+              l_tab_user_json(ind).NAME           := l_json_values.get(ind1).get_string();
+            WHEN 2 THEN
+              l_tab_user_json(ind).COURSE         := l_json_values.get(ind1).get_string();
+            WHEN 3 THEN                           
+              l_tab_user_json(ind).LEVEL_DISC     := l_json_values.get(ind1).get_number();
+            WHEN 4 THEN                           
+              l_tab_user_json(ind).DEF            := l_json_values.get(ind1).get_number();
+            ELSE
+              NULL;              
+          END CASE;
+          
+          DBMS_OUTPUT.PUT_LINE(' ELEMENT ' || ind1 || ' ' || l_json_values.get(ind1).get_type() );
+         
+          --Gets the Object type element
+          IF (l_json_values.get(ind1).IS_OBJECT)
+            AND NOT(l_json_values.get(ind1).IS_NULL) 
+          THEN
+            
+            DECLARE              
+              l_col_json json := json();
+            
+              l_collection_id TBEX1_COLLECTION_JSON.COLLECTION_ID%TYPE;
+              l_tb_size NUMBER;
+              
+            BEGIN
+               
+               l_col_json := json(l_json_values.get(ind1));
+               
+               --Aquiring current size of collection
+               IF l_tab_collection_json.count() = 0 THEN
+                 l_tb_size:= 1;
+               ELSE
+                 l_tb_size := l_tab_collection_json.count();
+               END IF;  
+                  
+               DBMS_OUTPUT.PUT_LINE(' **count:' || l_tb_size);
+               
+               --appending an element to the collection
+               l_tab_collection_json.EXTEND(1);
+               --
+               
+               BEGIN
+                 SELECT SEQ_TBEX1_COLLECTION_JSON.NEXTVAL INTO l_collection_id FROM DUAL;
+               END;
+               --
+               --populating collection
+               l_tab_collection_json(l_tb_size).COLLECTION_ID    := l_collection_id;
+               l_tab_collection_json(l_tb_size).USER_ID          := l_user_id;
+               l_tab_collection_json(l_tb_size).BOOK             := l_col_json.get('books').get_String();  --getting by Key_pair name, when not ARRAY.
+               l_tab_collection_json(l_tb_size).MUSIC            := l_col_json.get('music').get_String();
+               l_tab_collection_json(l_tb_size).ID_LAST_UPDATED  := USER;
+               l_tab_collection_json(l_tb_size).DTE_LAST_UPDATED := SYSDATE;
+                           
+            END;          
+          
+          --if looped element is an ARRAY
+          ELSIF (l_json_values.get(ind1).IS_ARRAY)
+            AND NOT(l_json_values.get(ind1).IS_NULL) 
+          THEN
+            DECLARE
+              l_col_json json := json();
+              l_col_list json_list;
+              
+              l_collection_id TBEX1_COLLECTION_JSON.COLLECTION_ID%TYPE;
+              l_tb_size NUMBER;
+                            
+            BEGIN
+            
+              l_col_list := json_list(l_json_values.get('collections'));
+            
+              --Aquiring current size of collection
+              IF l_tab_collection_json.count() = 0 THEN
+                l_tb_size:= 1;
+              ELSE
+                l_tb_size := l_tab_collection_json.count();
+              END IF; 
+              
+              -- Extending the collection size
+              l_tb_size := l_tb_size + l_col_list.count();
+              l_tab_collection_json.EXTEND(l_col_list.count());
+              
+  
+              FOR col1 IN 1..l_col_list.count()
+              LOOP
+                --get each element of the array
+                l_col_json := json(l_col_list.get(col1));
+                
+                --Fetch ID for child line
+               BEGIN
+                 SELECT SEQ_TBEX1_COLLECTION_JSON.NEXTVAL INTO l_collection_id FROM DUAL;
+               END;
+                
+                l_tab_collection_json(l_tb_size-(col1-1)).COLLECTION_ID     := l_collection_id;
+                l_tab_collection_json(l_tb_size-(col1-1)).USER_ID           := l_user_id;
+                l_tab_collection_json(l_tb_size-(col1-1)).BOOK              := l_col_json.get('books').get_String();
+                l_tab_collection_json(l_tb_size-(col1-1)).music             := l_col_json.get('music').get_String();
+                l_tab_collection_json(l_tb_size-(col1-1)).ID_LAST_UPDATED   := USER;
+                l_tab_collection_json(l_tb_size-(col1-1)).DTE_LAST_UPDATED  := SYSDATE;
+              END LOOP;
+              
+            
+            END;
+
           END IF;
 
-        END LOOP; --END l_sub_json
-                
-        DBMS_OUTPUT.PUT_LINE('Values of element(' ||ind||'): '||l_values_output);
-      
+        END LOOP; --END l_json_values l_tab_user_json
+        
+        
+        --COntinuing last element population
+        l_tab_user_json(ind).ID_LAST_UPDATED     := USER;
+        l_tab_user_json(ind).DTE_LAST_UPDATED    := SYSDATE;
+        
+        
+        DBMS_OUTPUT.PUT_LINE('Values of element(' ||ind||'): '||l_values_output
+          || '  -  Obj type: ' || l_json_list.get(ind).get_type        
+        );
+               
+        
       END IF;
      
 
-    END LOOP; --END l_json_list
+    END LOOP; --END l_json_list (ind)
     
+    --ISERTS INTO TABLE in father rows 
+    IF l_tab_user_json.COUNT() > 0 OR l_tab_user_json IS NOT NULL THEN
+      
+      FORALL j IN l_tab_user_json.FIRST .. l_tab_user_json.LAST
+        INSERT INTO TBEX1_USER_JSON VALUES l_tab_user_json(j);    
+      
+    END IF;
+    
+    --INSERTS INTO TABLE child rows
+    IF l_tab_collection_json.COUNT() >0 OR l_tab_collection_json IS NOT NULL THEN
+    
+      --FORALL j IN l_tab_collection_json.FIRST .. l_tab_collection_json.LAST
+      --  INSERT INTO TBEX1_COLLECTION_JSON VALUES l_tab_collection_json(j);
+    
+      dbms_output.put_line('****Printing l_tab_collection_json:');
+      FOR j IN 1..l_tab_collection_json.COUNT() LOOP
+        dbms_output.put_line( 'ROW' || j ||':' ||       
+          l_tab_collection_json(j).COLLECTION_ID     ||' - '||
+          l_tab_collection_json(j).USER_ID           ||' - '||
+          l_tab_collection_json(j).BOOK              ||' - '||
+          l_tab_collection_json(j).music             ||' - '||
+          l_tab_collection_json(j).ID_LAST_UPDATED   ||' - '||
+          l_tab_collection_json(j).DTE_LAST_UPDATED 
+        );
+        
+      END LOOP;
+
+    END IF;
+    
+    
+    COMMIT;
     
   END IF;  
   
